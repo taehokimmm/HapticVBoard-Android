dea901ca3414e31c3b46c95b7a4d4fed464390b8
yimjisu <60869086+yimjisu@users.noreply.github.com>
2024-07-26 17:59:50 +0900

    [add] study2 database

--- app/src/main/java/com/taehokimmm/hapticvboard_android/database/study2/Study2.kt
+++ app/src/main/java/com/taehokimmm/hapticvboard_android/database/study2/Study2.kt
@@ -1,4 +1,34 @@
 package com.taehokimmm.hapticvboard_android.database.study2
 
-class Study2 {
-}
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity("touch_metrics")
+data class TouchMetric(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    val testName: String,
+    val iteration: Int,
+    val touchStartX: Int,
+    val touchStartY: Int,
+    val touchEndX: Int,
+    val touchEndY: Int,
+
+    val touchStart: Char,
+    val touchEnd: Char,
+
+    val timestamp: Long,
+    val touchDuration: Long
+)
+
+@Entity(tableName = "test_metrics")
+data class TestMetric(
+    @PrimaryKey(autoGenerate = true) val uid : Int = 0,
+    val testName: String,
+    val iteration: Int,
+    val wpm: Double,
+    val accuracy: Double,
+    val timestamp: Long,
+    val touchMetrics: List<TouchMetric>
+)
